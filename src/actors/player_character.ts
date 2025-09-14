3e5747d95a2cb81373de763b6d160e59dada4e77
Boinga15 <realboinga@gmail.com>
2025-09-14 20:15:29 +0100

    Added player movement.

--- src/actors/player_character.ts
+++ src/actors/player_character.ts
@@ -4,6 +4,8 @@ import { Graphics } from "pixi.js";
 
 export class PlayerCharacter extends Actor {
     playerRef: Player
+
+    speed: number
     
     constructor(game: Game, x: number, y: number) {
         super(game);
@@ -15,5 +17,24 @@ export class PlayerCharacter extends Actor {
 
         // Graphics
         this.addChild(new Graphics().rect(-20, -20, 40, 40).fill("#00ff4cff"));
+
+        // Statistics
+        this.speed = 400;
+    }
+
+    update(deltaTime: number) {
+        super.update(deltaTime);
+
+        if (this.game.keys["KeyD"]) {
+            this.x = Math.min(480, Math.max(-480, this.x + (this.speed * deltaTime)));
+        } else if (this.game.keys["KeyA"]) {
+            this.x = Math.min(480, Math.max(-480, this.x - (this.speed * deltaTime)));
+        }
+
+        if (this.game.keys["KeyW"]) {
+            this.y = Math.min(480, Math.max(-480, this.y - (this.speed * deltaTime)));
+        } else if (this.game.keys["KeyS"]) {
+            this.y = Math.min(480, Math.max(-480, this.y + (this.speed * deltaTime)));
+        }
     }
 }
